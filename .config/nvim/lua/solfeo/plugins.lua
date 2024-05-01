f96979f0ac982ab6233f53749e4b9a726a49f334
Decliv <declivtosira@gmail.com>
2024-05-01 05:00:49 +0000

    eliminacion de plugins nvim

--- .config/nvim/lua/solfeo/plugins.lua
+++ .config/nvim/lua/solfeo/plugins.lua
@@ -2,67 +2,50 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	-- Packer
-	-- use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
 	-- Tema 
-	-- use({"Shatur/neovim-ayu", as = "ayu"})
+  use({"Shatur/neovim-ayu", as = "ayu"})
 	
 	-- -- Gestor de Archivos
-	-- use({
-	-- 	"nvim-telescope/telescope.nvim",
-	-- 	tag = "0.1.6",
-	-- 	requires = {{"nvim-lua/plenary.nvim"}},
-	-- })
+	use({
+	  "nvim-telescope/telescope.nvim",
+	 	tag = "0.1.6",
+	 	requires = {{"nvim-lua/plenary.nvim"}},
+  })
 
-	-- -- Coloreado de Sintaxis 
-	-- use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
+	-- Coloreado de Sintaxis 
+	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 	
-	-- -- Intellisense
-	-- use {
-  --   'VonHeikemen/lsp-zero.nvim',
-  --   branch = 'v3.x',
-  --   requires = {
-  --     --- Uncomment the two plugins below if you want to manage the language servers from neovim
-  --     -- {'williamboman/mason.nvim'},
-  --     -- {'williamboman/mason-lspconfig.nvim'},
+	-- Motor Aucompletado
+  use { "neoclide/coc.nvim", branch = 'release' }
+	
+	-- Autopairs 
+	use{
+	 	"windwp/nvim-autopairs",
+    event = "InsertEnter",
+	 	config = function()
+	 		require("nvim-autopairs").setup {}
+	 	end
+	}
 
-  --     {'neovim/nvim-lspconfig'},
-  --     {'hrsh7th/nvim-cmp'},
-  --     {'hrsh7th/cmp-nvim-lsp'},
-  --     {'L3MON4D3/LuaSnip'},
-  --   }
-  -- }
+  -- Status Line
+   use{
+     'nvim-lualine/lualine.nvim',
+     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 
-	-- -- Autopairs 
-	-- use({
-	-- 	"windwp/nvim-autopairs",
-	-- 	config = function()
-	-- 		require("configs.autopairs")
-	-- 	end,
-	-- })
-
-  -- -- Background Transparent
-  -- use(
-  -- 	"tribela/vim-transparent"
-  -- )
-
-  -- -- Status Line
-  -- use{
-  --   'nvim-lualine/lualine.nvim',
-  --   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  -- }
-
-  -- -- Gestor de Archivos (Tree)
-  -- use {
-  -- "nvim-neo-tree/neo-tree.nvim",
-  --   branch = "v3.x",
-  --   requires = { 
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-  --     "MunifTanjim/nui.nvim",
-  --     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  --   }
-  -- }
+  -- Gestor de Archivos (Tree)
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+      requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
   
 end)
   
